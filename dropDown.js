1259c4e000e0e641a07f8e8f9e62962e84b868a6
johalloran01 <65722241+johalloran01@users.noreply.github.com>
2024-01-30 19:11:27 -0500

    Adjusted Drop Down Menu
    Added a border radius to drop down button, and content

--- dropDown.js
+++ dropDown.js
@@ -1,11 +1,11 @@
 function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("eduDropdown").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var dropdowns = document.getElementsByClassName("edu-dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
